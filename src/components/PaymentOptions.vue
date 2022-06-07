75d3f3c8a8de59f8d7c69d7e27191d2f306fc9dc
Jeden1102 <bum123@o2.pl>
2022-06-07 23:17:31 +0200

    hotfix

--- src/components/PaymentOptions.vue
+++ src/components/PaymentOptions.vue
@@ -43,10 +43,10 @@ import { mapState } from 'vuex';
             return {
                 deliveryMethods:[
                     {
-                        img:"/src/assets/payu.png",text:"PayU",name:"payu"
+                        img:"src/assets/payu.png",text:"PayU",name:"payu"
                     },
                     {
-                        img:"/src/assets/Przelew.png",text:"Przelew bankowy - zwykły",name:"przelew"
+                        img:"src/assets/Przelew.png",text:"Przelew bankowy - zwykły",name:"przelew"
                     },
                 ],
                 deliveryMethodsSelect:[
