375f06d45814f5f0de3d19430789860660898ffd
Jeden1102 <bum123@o2.pl>
2022-06-08 10:50:58 +0200

    new api addres

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist origin gh-pages-2
 
 cd -
