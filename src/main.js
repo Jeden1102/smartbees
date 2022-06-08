5748d9cac1f4b6ba04048b1551bb4cd859703cfc
Jeden1102 <bum123@o2.pl>
2022-06-08 11:15:50 +0200

    deploy

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import store  from './store'
-import './index.css'
-import Vuelidate from 'vuelidate'
-import App from './App.vue'
-
-createApp(App).use(store).mount('#app')
